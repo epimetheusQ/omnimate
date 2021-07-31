7fdfc58ed097dfdf819f47e510df4ef4ba710ffe
wuerqiQs <it_527@163.com>
2021-07-31 16:56:29 +0800

    代码上传

--- src/Utils/Arr.php
+++ src/Utils/Arr.php
@@ -1,25 +1,84 @@
 <?php
 /**
  * php数组日常使用的扩展类（方案类似laravel Collection）
- * @author wuerqiQs
+ * @author 527
  * @license MIT
  * @link https://packagist.org/packages/epimetheus/omnimate
- * @link https://github.com/epimetheusQ?tab=repositories
  */
 namespace Epimetheus\Omnimate\Utils;
 
 class Arr
 {
-    public function groupKey(array $data, string $key, array $fields = [])
+    /**
+     * 二维数组分组展示
+     * @param array $list 需要处理的array
+     * @param string $key 分组的key
+     * @param array $fields 需要展示的字段
+     * @return array
+     */
+    public static function multiGroupBy(array $list = [], string $key, array $fields = []) : array
     {
-        $res = [];
-        foreach ($data as $index => $val) {
-            if (! isset($val[$key])) {
-                return $res;
-            }
+        $multiArr = [];
 
-            $res[$val[$key]][] = $val;
+        foreach ($list as $index => $val) {
+            if (! $fields) {
+                $multiArr[$val[$key]][] = $val;
+            } else {
+                foreach ($fields as $field) {
+                    $childVal[$field] = isset($val[$field]) ? $val[$field] : '';
+                }
+                $multiArr[$val[$key]][] = $childVal ?? [];
+            }
         }
-        return $res;
+
+        return $multiArr;
+    }
+
+    /**
+     * 二维数组过滤参数（多用于给前端提供筛选数据）
+     * @param array $array
+     * @param array $fields
+     * @return array
+     */
+    public static function multiFilter(array $array, array $fields = []) : array
+    {
+        return array_map(function ($val) use (&$fields) {
+            if ($fields) {
+                foreach ($fields as $field) {
+                    $arr[$field] = isset($val[$field]) ? $val[$field] : '';
+                }
+                return $arr;
+            }
+            return $val;
+        }, $fields);
+    }
+
+    /**
+     * 根据where条件，在二维数组中模糊搜索数据
+     * @param array $array 需要搜索的数据
+     * @param array $where where搜索条件
+     * @param array $fields 需要展示的字段
+     * @return array
+     */
+    public static function multiFuzzyWhere(array $array, array $where, array $fields = []) : array
+    {
+
+    }
+
+    /**
+     * 根据where条件，准确搜索数据
+     * @param array $array
+     * @param array $where
+     * @param array $fields
+     * @return array
+     */
+    public static function multiWhere(array $array, array $where, array $fields = []) : array
+    {
+
+    }
+
+    public static function multiPluck(array $array, $simles = '', $key = '') : array
+    {
+
     }
 }
